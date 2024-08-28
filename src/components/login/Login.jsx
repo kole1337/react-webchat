9704b25ea63e9348bbab90b89a765941a2465330
Nikola Parushev <56691310+kole1337@users.noreply.github.com>
2024-08-29 00:22:01 +0100

    registration and login
    it is working at the backend

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,13 +1,21 @@
 import { useState, preventDefault } from "react"
 import "./login.css"
 import { toast } from "react-toastify"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
+import { setDoc, doc } from "firebase/firestore"
+import { auth } from "/src/lib/firebase.js"
+import { db } from "/src/lib/firebase.js"
+import {getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage"
+import upload from "../../lib/upload"
 
 const Login = () => {
 
     const [avatar, setAvatar] = useState({
         file: null,
         url: ""
-    })
+    });
+
+    const [loading, setLoading] = useState(false)
 
     const handleAvatar = e => {
         if (e.target.files[0]) {
@@ -18,27 +26,79 @@ const Login = () => {
         }
     }
 
-    const handleLogin = e =>{
+    const handleLogin = async (e) => {
         e.preventDefault()
-        
+        setLoading(true)
+
+        const formData = new FormData(e.target)
+
+        const { email, password } = Object.fromEntries(formData);
+
+        try{
+            
+            await signInWithEmailAndPassword(auth, email, password);
+            toast.success("Welcome back!");
+
+        }catch(error){
+            console.log(error);
+            toast.error(error.message);
+        }finally{
+            setLoading(false)
+        }
+
+    }
+
+    const handleRegister = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+        const formData = new FormData(e.target)
+
+        const { username, email, password } = Object.fromEntries(formData);
+
+        try {
+            
+            const res = await createUserWithEmailAndPassword(auth, email, password);
+
+            const imgUrl = await upload(avatar.file);
+
+            await setDoc(doc(db, "users", res.user.uid),{
+                username,
+                email,
+                avatar:imgUrl,
+                id: res.user.uid,
+                blocked: [],
+            });
+
+            await setDoc(doc(db, "userschats", res.user.uid),{
+                chats: []
+            });
+
+            toast.success("Account created!")
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        } finally{
+            setLoading(false)
+        }
+
     }
 
     return (
         <div className="login">
             <div className="item">
                 <h2>Welcome back,</h2>
-                
+
                 <form onSubmit={handleLogin}>
                     <input type="email" name="email" id="" placeholder="Email" />
                     <input type="password" name="password" id="" placeholder="Password" />
-                    <button>Sign In</button>
+                    <button disabled={loading}>{ loading ? "Loading" : "Sign In"}</button>
                 </form>
             </div>
             <div className="separator">
             </div>
             <div className="item">
                 <h2>Create an account</h2>
-                <form>
+                <form onSubmit={handleRegister}>
                     <label htmlFor="file">
                         <img src={avatar.url || "./avatar.png"} alt="" />
                         Upload an image
@@ -47,7 +107,7 @@ const Login = () => {
                     <input type="text" name="username" id="" placeholder="Username" />
                     <input type="email" name="email" id="" placeholder="Email" />
                     <input type="password" name="password" id="" placeholder="Password" />
-                    <button>Sign In</button>
+                    <button disabled={loading}>{ loading ? "Loading" : "Sign Up"}</button>
                 </form>
             </div>
 
